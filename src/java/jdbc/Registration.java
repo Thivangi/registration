18cf95a552513cec9e20778612d2d7e571f50ac8
thivangi <thivangikumari@gmail.com>
2020-07-09 19:57:02 +0530

    database connection

---
+++ src/java/jdbc/Registration.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author AsusVivo
+ */
+public class Registration extends HttpServlet {
+
+    public Registration()
+    {
+        super();
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+   
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       try{
+           String name=request.getParameter("user");
+           String password=request.getParameter("password");
+           String sql="insert into registration(name,password) values(?,?)";
+           Class.forName("com.mysql.jdbc.Driver");
+           try {
+               Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/portal","root","");
+               PreparedStatement ps=conn.prepareStatement(sql);
+               ps.setString(1,name);
+               ps.setString(2, password);
+               ps.executeUpdate();
+               PrintWriter out=response.getWriter();
+               out.println("you have succesully registered");
+               
+               
+           } catch (SQLException ex) {
+               Logger.getLogger(Registration.class.getName()).log(Level.SEVERE, null, ex);
+           }
+           
+       }
+       
+           
+               
+           catch (ClassNotFoundException ex) {
+            Logger.getLogger(Registration.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
